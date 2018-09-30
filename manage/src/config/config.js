6a017006b0caabd68adcffdc9725b3c065171324
程路路 <919903021@qq.com>
2018-09-30 13:56:18 +0800

    修改scoket

--- manage/src/config/config.js
+++ manage/src/config/config.js
@@ -2,9 +2,12 @@
 let socketUrl = '';
 let host = window.location.host || 'http://localhost:3000';
 if(process.env.NODE_ENV == 'development'){
-  socketUrl = host
+  socketUrl = 'http://localhost:3000'
 }else if(process.env.NODE_ENV == 'production'){
-  socketUrl = host
+  console.log('-----')
+  console.log(process.env.NODE_ENV)
+  socketUrl = 'http://39.106.63.9:80'
+  // socketUrl = 'http://39.107.89.167:80'
 }
 
 export {
