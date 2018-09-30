79b9174dad9b3e183714728362e5ec547194f92d
程路路 <919903021@qq.com>
2018-09-30 10:19:46 +0800

    修改环境变量

--- manage/src/config/config.js
+++ manage/src/config/config.js
@@ -1,11 +1,10 @@
 
 let socketUrl = '';
+let host = window.location.host || 'http://localhost:3000';
 if(process.env.NODE_ENV == 'development'){
-  socketUrl = 'http://localhost:3000'
+  socketUrl = host
 }else if(process.env.NODE_ENV == 'production'){
-  console.log('-----')
-  console.log(process.env.NODE_ENV)
-  socketUrl = 'http://39.107.89.167:80'
+  socketUrl = host
 }
 
 export {
