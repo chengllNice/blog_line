72400aa43047729924616fcbd77e7103705af7b7
程路路 <919903021@qq.com>
2018-09-30 13:48:47 +0800

    修改scoket

--- vue/src/config/config.js
+++ vue/src/config/config.js
@@ -1,11 +1,9 @@
 
-let socketUrl = '';
+let socketUrl = window.location.host || 'http://localhost:3000';
 if(process.env.NODE_ENV == 'development'){
   socketUrl = 'http://localhost:3000'
 }else if(process.env.NODE_ENV == 'production'){
-  console.log('-----')
-  console.log(process.env.NODE_ENV)
-  socketUrl = 'http://39.107.89.167:80'
+  socketUrl = 'http://39.106.63.9:80'
 }
 
 export {
